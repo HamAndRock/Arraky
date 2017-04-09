d9470db64fa69aa5b2ac396410104a2fee9f8cf5
Jiří Svěcený <sitole20@gmail.com>
2017-04-09 13:05:57 +0200

    Update Autoloader.php

--- Library/Basic/Autoloader.php
+++ Library/Basic/Autoloader.php
@@ -7,11 +7,6 @@
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  */
 
-/**
- * @param  string  $Library     [Knihovna se souborem]
- * @param  string  $Destination [Umístění souboru v knihovně]
- * @return boolean              [Úspěšné načtení knihovny]
- */
 function autoloader($Library, $Destination)
 {
 	$File = ROOT . "/Core/Libraries/" . $Library . "/" . $Destination;
@@ -19,7 +14,10 @@ function autoloader($Library, $Destination)
 	if (file_exists($File))
 	{
 		require_once $File;
+		return true;
 	}
-
-	return (file_exists($File) ? True : False);
+	else
+	{
+		return false;
+	}
 }
