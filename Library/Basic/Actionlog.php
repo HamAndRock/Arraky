0c3c35e8b4805f760debb621658e491e5df7515a
Jiří Svěcený <sitole20@gmail.com>
2017-04-12 10:33:45 +0200

    Create Actionlog.php
    Funkce pro záznam událostí v aplikaci do unikátních souborů, které se ukládají v rozmezí dnů.

---
+++ Library/Basic/Actionlog.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @package Arraky
+ * @copyright 2017 Jiří Svěcený
+ * @author Jiří Svěcený <sitole20@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ */
+
+/**
+ * @param integer $Code    [Kod události]
+ * @param string  $Message [Jednoduchá a struční zpráva]
+ */
+function actionlog($Code, $Message)
+{
+	$File    = ROOT . "/Core/Archiv/Log/" . date("Y-m-d") . ".txt";
+	$Message = date("d/m/Y i:H:s") . " [$Code] $Message\n";
+
+	$Write = fopen($File, "a");
+	fwrite($Write, $Message);
+	fclose($Write);
+}
