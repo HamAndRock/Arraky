a31ec56e6775f38d32c8cb0479ddded02ab4b2aa
Jiří Svěcený <sitole20@gmail.com>
2017-04-17 19:06:00 +0200

    Create BBCode.php

---
+++ Library/Chains/BBCode.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package Arraky
+ * @copyright 2017 Jiří Svěcený
+ * @author Jiří Svěcený <sitole20@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ */
+ 
+class BBCode
+{
+
+}
